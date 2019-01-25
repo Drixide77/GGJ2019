9b2f754150d31e5b2aa58a8a23e6cc77b266f763
Adrià <adria.abella.93@gmail.com>
2019-01-25 23:07:24 +0100

    Main menu skeleton finished. WIP on the Main games scene.

---
+++ GGJ2019/Assets/Scripts/Fader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    public Image fadeImage;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoFade(float alpha, float duration, Action<bool> callback)
+    {
+        StartCoroutine(DoFadeCoroutine(alpha, duration, callback));
+    }
+
+    IEnumerator DoFadeCoroutine(float alpha, float duration, Action<bool> callback) {
+        float startAlpha = fadeImage.color.a;
+        float endAlpha = alpha;
+
+        for (float f = 0.0f; f <= duration; f += Time.deltaTime)
+        {
+            Color c = fadeImage.color;
+            c.a = Mathf.Lerp(startAlpha, endAlpha, f/duration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        Color final = fadeImage.color;
+        final.a = endAlpha;
+        fadeImage.color = final;
+        callback.Invoke(true);
+    }
+}
---
+++ GGJ2019/Assets/Scripts/MenuController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public Fader fader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartGamePressed()
+    {
+        Debug.Log("Game Start Pressed");
+        fader.DoFade(1.0f, 0.2f, (finish) =>
+            {
+                SceneManager.LoadScene("Main", LoadSceneMode.Single);
+            }
+        );
+    }
+
+    public void OnHowToPressed()
+    {
+        Debug.Log("HowToPressed");
+
+    }
+
+    public void OnExitPressed()
+    {
+        Debug.Log("HowToPressed");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
