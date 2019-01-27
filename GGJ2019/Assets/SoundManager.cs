e2feba443f718391b9c168b7166efd4c68ae1598
Max72590 <max-725@hotmail.com>
2019-01-27 09:20:02 +0100

    Updated main

---
+++ GGJ2019/Assets/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public GameObject GameMusic;
+    public GameObject StarSounds;
+    //public Clips
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayStarSound() {
+        
+    }
+
+    public void PlayWinSound() {
+
+    }
+
+}
