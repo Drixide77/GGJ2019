e588663ef14a30e4416b0cab08e8da5cbb9cde6a
Adrià <adria.abella.93@gmail.com>
2019-02-03 11:49:52 +0100

    Fixed title menu music not playing.

--- GGJ2019/Assets/TestDemo/TestScripts/MenuMusicController.cs
+++ GGJ2019/Assets/TestDemo/TestScripts/MenuMusicController.cs
@@ -7,10 +7,15 @@ public class MenuMusicController : MonoBehaviour
 
     private AudioSource audioSource;
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
